780fd10d5c8d9d7c2f0fce7d4ae1245721af9337
Anastasia <great-swindler@mail.ru>
2019-09-10 11:09:48 +0300

    Finalized project

---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Track from '../Track/Track';
+import './TrackList.css'
+
+
+class TrackList extends React.Component {
+      render() { 
+        return (
+        <div className="TrackList">
+            {this.props.tracks.map(track => {
+               return <Track 
+                onAdd={this.props.onAdd}
+                key={track.id}
+                track={track} 
+                onRemove={this.props.onRemove}
+                isRemoval={this.props.isRemoval}
+                />
+            })}
+        </div> 
+        );
+    }
+}
+ 
+export default TrackList;
